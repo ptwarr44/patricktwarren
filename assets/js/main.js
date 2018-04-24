7ef74abc8bc6761a2a4fcc0837020ba4daab1dbe
ptwarr44 <ptwarr44@users.noreply.github.com>
2018-04-23 22:39:31 -0400

    Final edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,3 +117,24 @@ $("#main-heading").html(function changeHeaderOnTime(){
 	else
 		return document.getElementById("main-heading").innerHTML = "Good Evening";
 });
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
